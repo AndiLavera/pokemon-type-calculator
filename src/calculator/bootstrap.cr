88e12d712410b58b48411c0729a23f2612f43812
Andrew <awcrotwell@gmail.com>
2021-01-19 14:54:01 -0500

    Serverless is working

--- src/calculator/bootstrap.cr
+++ src/calculator/bootstrap.cr
@@ -12,15 +12,18 @@ def serverless
   runtime.register_handler("httpevent") do |input|
     io = IO::Memory.new
     req = Lambda::Builder::HTTPRequest.new(input)
-    res = HTTP::Server::Response.new(io)
-    context = HTTP::Server::Context.new(req, res)
+    context = HTTP::Server::Context.new(req, HTTP::Server::Response.new(io))
 
     ADI.container.athena_routing_route_handler.handle context
 
-    pp res
+    res = Lambda::Builder::HTTPResponse.new(
+      context.response.status_code,
+      io.to_s.split("\n").last,
+    )
 
-    # not super efficient, serializing to JSON string and then parsing, simplify this
-    JSON.parse Lambda::Builder::HTTPResponse.new(res.status_code, res.output.to_s).to_json
+    # not super efficient
+    # serializing to JSON string and then parsing so we return JSON::Any
+    JSON.parse(res.to_json)
   end
 
   pp "Serverless is waiting for events"
